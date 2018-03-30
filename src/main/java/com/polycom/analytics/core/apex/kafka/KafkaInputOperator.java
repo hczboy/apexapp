ca305f15403ade0c48c183c8ce1353de42c9a1e9
czhhu <chengzhe.hu@polycom.com>
2018-03-30 17:44:14 +0800

    enhance exception handling in kafkaInputOperator

--- src/main/java/com/polycom/analytics/core/apex/kafka/KafkaInputOperator.java
+++ src/main/java/com/polycom/analytics/core/apex/kafka/KafkaInputOperator.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import org.apache.apex.malhar.kafka.AbstractKafkaInputOperator;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.alibaba.fastjson.JSON;
 import com.datatorrent.api.DefaultOutputPort;
@@ -11,6 +13,7 @@ import com.datatorrent.lib.util.KeyValPair;
 
 public class KafkaInputOperator extends AbstractKafkaInputOperator
 {
+    private static final Logger log = LoggerFactory.getLogger(KafkaInputOperator.class);
 
     public final transient DefaultOutputPort<Map<String, Object>> output1 = new DefaultOutputPort<>();
     public final transient DefaultOutputPort<KeyValPair<String, byte[]>> hdfsOut = new DefaultOutputPort<>();
@@ -20,7 +23,16 @@ public class KafkaInputOperator extends AbstractKafkaInputOperator
     protected void emitTuple(String cluster, ConsumerRecord<byte[], byte[]> message)
     {
         String msg = new String(message.value());
-        Map<String, Object> msgMap = JSON.parseObject(msg);
+        Map<String, Object> msgMap = null;
+        try
+        {
+            msgMap = JSON.parseObject(msg);
+        }
+        catch (Exception e)
+        {
+            log.error("Failed to parse kafka message:{} for topic: {}", msg, message.topic());
+            return;
+        }
         output1.emit(msgMap);
         output2.emit(msgMap);
         String topic = message.topic();
